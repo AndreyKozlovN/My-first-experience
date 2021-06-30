4188d3cb66f424aa33a8277a8ef11db9856e6110
Andrey <kozlov205an@gmail.com>
2021-06-30 15:26:47 +0400

    homework 2

--- script.js
+++ script.js
@@ -9,13 +9,30 @@ const personalMovieDB = {
         genres: [],
         privat: false
 };
-const a = prompt("Один из последних просмотренных фильмов", ''),
-      b = +prompt("На сколько баллов вы его оцените?", ''),
-      c = prompt("Один из последних просмотренных фильмов", ''),
-      d = +prompt("На сколько баллов вы его оцените?", '');
- 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-    
 
-console.log(personalMovieDB);    
+if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман');
+} else {
+        alert("Произошла ошибка");
+}
+
+
+
+for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов", ''),
+              b = +prompt("На сколько баллов вы его оцените?", '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB[a] = b; 
+                console.log('done');
+        } else {
+                console.log('error');
+                i--; 
+        }
+
+}
+console.log(personalMovieDB);
